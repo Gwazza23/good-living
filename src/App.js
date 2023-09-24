f51811082da3bb80205c637488684b222abffc92
Gwazza23 <weerawarnagayan@gmail.com>
2023-09-24 12:33:55 +0100

    Setup React Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
