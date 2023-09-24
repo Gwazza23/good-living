f4e34615e642c6c8c52702d12eda54f8ffaf89f7
Gwazza23 <weerawarnagayan@gmail.com>
2023-09-24 14:07:00 +0100

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Nav from "./Pages/Nav/Nav";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ src/Pages/Nav/Nav.js
@@ -0,0 +1,28 @@
+import "./Nav.css";
+import { NavLink, Outlet } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+
+function Nav() {
+  return (
+    <>
+      <nav>
+        <div className="nav-logo-container">
+          <NavLink to={"/"}>
+            <h1>
+              Good<span>Living</span>
+            </h1>
+          </NavLink>
+        </div>
+        <div className="nav-links">
+          <NavLink to={"/categories"}>Categories</NavLink>
+          <button>
+            <BsCart3 />
+          </button>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default Nav;
